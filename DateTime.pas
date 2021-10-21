2576bf4b742d0bac11b7f4ab4da0b75b225dce6b
Олег Новиков <gthxbr2004@gmail.com>
2021-10-21 08:59:05 +0300

    Correct DateTime.pas. Only leap year

---
+++ DateTime.pas
@@ -0,0 +1,14 @@
+﻿function IsLeapYear: boolean;
+begin
+  var a := DateTime.Now.Year;
+  if ((a mod 4 = 0) and (a mod 100 > 0)) or (a mod 400 = 0) then
+    Result := true
+  else
+    Result := false;
+end;
+
+
+
+begin
+  
+end.
